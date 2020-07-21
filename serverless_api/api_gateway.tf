1fbcfb1a86ac706056a5a3b5f4dbe5bcfdacbf40
Michel Lacle <mlacle@progressrail.com>
2020-07-21 18:21:54 -0400

    add API endpoint

--- serverless_api/api_gateway.tf
+++ serverless_api/api_gateway.tf
@@ -1,57 +1,57 @@
-//
-//resource "aws_api_gateway_rest_api" "this" {
-//  name = var.name
-//}
-//
-//resource "aws_api_gateway_deployment" "this" {
-//  rest_api_id = aws_api_gateway_rest_api.this.id
-//  stage_name  = "prod"
-//
-//  lifecycle {
-//    create_before_destroy = true
-//  }
-//
-//  depends_on = [aws_api_gateway_method.this]
-//}
-//
-//resource "aws_api_gateway_stage" "this" {
-//  stage_name    = "prod"
-//  rest_api_id   = aws_api_gateway_rest_api.this.id
-//  deployment_id = aws_api_gateway_deployment.this.id
-//}
-//
-//
-//resource "aws_api_gateway_resource" "this" {
-//  path_part   = "commitMessages"
-//  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
-//  rest_api_id = aws_api_gateway_rest_api.this.id
-//}
-//
-//resource "aws_api_gateway_method" "this" {
-//  rest_api_id   = aws_api_gateway_rest_api.this.id
-//  resource_id   = aws_api_gateway_resource.this.id
-//  http_method   = "GET"
-//  authorization = "NONE"
-//
-//
-//}
-//
-//
-//resource "aws_api_gateway_integration" "this" {
-//  rest_api_id             = aws_api_gateway_rest_api.this.id
-//  resource_id             = aws_api_gateway_resource.this.id
-//  http_method             = aws_api_gateway_method.this.http_method
-//  integration_http_method = "POST"
-//  type                    = "AWS_PROXY"
-//  uri                     = aws_lambda_function.this.invoke_arn
-//}
+resource "aws_api_gateway_rest_api" "this" {
+  name = var.name
+}
+
+resource "aws_api_gateway_deployment" "this" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  stage_name = "prod"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [aws_api_gateway_method.this]
+}
+
+/*
+resource "aws_api_gateway_stage" "this" {
+  stage_name    = "prod"
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  deployment_id = aws_api_gateway_deployment.this.id
+}
+*/
+
+resource "aws_api_gateway_resource" "this" {
+  path_part   = "commitMessages"
+  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.this.id
+}
+
+resource "aws_api_gateway_method" "this" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.this.id
+  http_method   = "GET"
+  authorization = "NONE"
 
 
-//resource "aws_lambda_permission" "this" {
-//  statement_id  = "AllowExecutionFromAPIGateway"
-//  action        = "lambda:InvokeFunction"
-//  function_name = aws_lambda_function.this.function_name
-//  principal     = "apigateway.amazonaws.com"
-//
-//  source_arn = "${data.aws_api_gateway_rest_api.this.execution_arn}/*/*/*"
-//}
+}
+
+
+resource "aws_api_gateway_integration" "this" {
+  rest_api_id             = aws_api_gateway_rest_api.this.id
+  resource_id             = aws_api_gateway_resource.this.id
+  http_method             = aws_api_gateway_method.this.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.this.invoke_arn
+}
+
+
+resource "aws_lambda_permission" "this" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.this.execution_arn}/*/*/*"
+}
