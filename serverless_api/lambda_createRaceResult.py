9adc73938ea0d4d2dbc8e32b2a89683dbff1fa53
Michel Lacle <mlacle@progressrail.com>
2020-07-22 16:11:46 -0400

    create race result inputs and pass data to API

--- serverless_api/lambda_createRaceResult.py
+++ serverless_api/lambda_createRaceResult.py
@@ -16,16 +16,14 @@ def send_to_slack(message):
 def main(event, context):
     send_to_slack("```" + json.dumps(event) + "```")
 
-    raceResults = [
-        {'date': '07/22/2020',
-         'driver': 'Lewis Hamilton',
-         'track': 'Austria',
-         'position': '1'},
-        {'date': '07/22/2020',
-         'driver': 'Max Verstappen',
-         'track': 'Austria',
-         'position': '2'}
-    ]
+    body = json.loads(event['body'])
+
+    date = body['date']
+    driver = body['driver']
+    track = body['track']
+    position = body['position']
+
+    # save in dynamo db
 
     response = {
         'statusCode': 200,
@@ -33,7 +31,7 @@ def main(event, context):
             "x-custom-header": "my custom header value",
             "Access-Control-Allow-Origin": "*"
         },
-        'body': json.dumps(raceResults)
+        'body': date
     }
 
     send_to_slack("```" + json.dumps(response) + "```")
